4fdc9df90db2083d0cf78fb24cf952741b7e9c5d
audrie & john <student@epicodus.com>
2015-06-08 10:46:19 -0700

    Passes spec for save and equals method for task class.

--- lib/task.rb
+++ lib/task.rb
@@ -11,8 +11,15 @@ class Task
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch('description')
-      tasks.push(Task.new({:desription => description}))
+      list_id = task.fetch('list_id').to_i()
+      tasks.push(Task.new({:description => description, :list_id => list_id}))
     end
     tasks
-  end   
+  end
+  define_method(:save) do
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+  end
+  define_method(:==) do |another_task|
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -6,4 +6,18 @@ describe(Task) do
       expect(Task.all()).to(eq([]))
     end
   end
+  describe('#save') do
+    it('adds a task to the array of saved tasks') do
+      test_task = Task.new({:description => 'wash the dog', :list_id => 1})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+  describe('#==') do
+    it('is the same task if it has the same description and list id') do
+      task1 = Task.new({:description => 'learn SQL', :list_id => 1})
+      task2 = Task.new({:description => 'learn SQL', :list_id => 1})
+      expect(task1).to(eq(task2))
+    end
+  end
 end
