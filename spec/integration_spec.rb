d73fdbf0725322ec3177d151358c17959f4f6e88
john young & debbie balbo <student@epicodus.com>
2015-06-11 14:34:21 -0700

    first integration path spec passes

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -7,8 +7,16 @@ set(:show_exceptions, false)
 describe('the to do website path', {:type => :feature}) do
   it('allows users to create lists of different tasks') do
     visit('/')
-    fill_in('name', :with => 'Epicodus Stuff')
+    fill_in('name', :with => 'Epicodus')
     click_button('Add')
-    expect(page).to have_content('Epicodus Stuff')
+    expect(page).to have_content('Epicodus')
+    click_link('Epicodus')
+    expect(page).to have_content("Epicodus")
   end
 end
+# describe('the path to a list\'s page', {:type => :feature}) do
+#   it('allows user to go to the individual list page') do
+#     visit('/')
+#
+#   end
+# end
