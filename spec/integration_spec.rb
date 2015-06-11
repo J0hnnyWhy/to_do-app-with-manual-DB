e654d2183752e057259062687bbc3fec4f65f86f
john young & debbie balbo <student@epicodus.com>
2015-06-11 15:31:52 -0700

    second integration spec passes for list page path

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -9,14 +9,18 @@ describe('the to do website path', {:type => :feature}) do
     visit('/')
     fill_in('name', :with => 'Epicodus')
     click_button('Add')
-    expect(page).to have_content('Epicodus')
     click_link('Epicodus')
     expect(page).to have_content("Epicodus")
   end
 end
-# describe('the path to a list\'s page', {:type => :feature}) do
-#   it('allows user to go to the individual list page') do
-#     visit('/')
-#
-#   end
-# end
+describe('the list page path', {:type => :feature}) do
+  it('allows user to add tasks in a list') do
+    visit('/')
+    fill_in('name', :with => 'Epicodus')
+    click_button('Add')
+    click_link('Epicodus')
+    fill_in('Add a Task to this List', :with => 'get out of bed')
+    click_button('Add')
+    expect(page).to have_content('get out of bed')
+  end
+end
